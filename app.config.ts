1621f8b461865b317ba66049e791bf79c11f1151
Rafael Fletes <rfletes45@gmail.com>
2026-04-16 20:21:34 -0400

    build 39: bump iOS build number

--- app.config.ts
+++ app.config.ts
@@ -15,7 +15,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     supportsTablet: true,
     bundleIdentifier: "com.vibeapp.mobile",
     googleServicesFile: "./GoogleService-Info.plist",
-    buildNumber: "38",
+    buildNumber: "39",
     infoPlist: {
       // Camera & Microphone permissions
       NSCameraUsageDescription: "Vibe needs camera access for video calls",
