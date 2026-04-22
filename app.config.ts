305eb1b0458183a8798f996047bd5621beaa6961
Rafael Fletes <rfletes45@gmail.com>
2026-04-21 21:43:31 -0400

    build 47: release current github head to TestFlight

--- app.config.ts
+++ app.config.ts
@@ -15,7 +15,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     supportsTablet: true,
     bundleIdentifier: "com.vibeapp.mobile",
     googleServicesFile: "./GoogleService-Info.plist",
-    buildNumber: "44",
+    buildNumber: "47",
     infoPlist: {
       // Camera & Microphone permissions
       NSCameraUsageDescription: "Vibe needs camera access for video calls",
